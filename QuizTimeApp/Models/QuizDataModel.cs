270f4896463307e5b231751da1282169135be9b0
MrLeafStick <klt@aarhustech.dk>
2020-07-14 14:02:13 +0200

    Initilize Project - Quiz App

---
+++ QuizTimeApp/Models/QuizDataModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizTimeApp.Models
+{
+    public class QuizDataModel
+    {
+        public int QuizId { get; set; }
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
